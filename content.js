e91b806a71657a20dfb7dc4c2199e24c2a236741
Christian Avi Bulan <43099141+christianavi@users.noreply.github.com>
2021-01-25 12:37:01 +0800

    Vanilla ✨

--- content.js
+++ content.js
@@ -1,25 +1,27 @@
-console.log("Removing ads from " + window.location.host)
 var yahoo = window.location.host.includes("search.yahoo");
 
 
 if (yahoo == false) {
     if (window.location.host == "www.google.com"){
+        console.log("🍦 Removing ads from " + window.location.host)
         document.getElementById("taw").style.display = "none"
-        console.log("Ads have been cleared.")
+        console.log("🍦 Ads have been cleared.")
     }
     else if (window.location.host == "www.bing.com"){
+        console.log("🍦 Removing ads from " + window.location.host)
         document.getElementsByClassName("b_ad")[0].style.display = "none"
         document.getElementsByClassName("b_adBottom")[0].style.display = "none"
-        console.log("Ads have been cleared.")
+        console.log("🍦 Ads have been cleared.")
     }
 }
 
 else if (yahoo == true){
+    console.log("🍦 Removing ads from " + window.location.host)
     document.getElementsByClassName("searchCenterTopAds")[0].style.display = "none"
     document.getElementsByClassName("searchRightBottomAds")[0].style.display = "none"
     document.getElementsByClassName("searchCenterBottomAds")[0].style.display = "none"
-    console.log("Ads have been cleared.")
+    console.log("🍦 Ads have been cleared.")
 }
 else {
-    console.log("Bro, I don't know what is happening.")
+    console.log("🍦 Apparently, this is one of the unforseen events.")
 }
